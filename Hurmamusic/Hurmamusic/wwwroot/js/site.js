209eb0a4a13af52d153c52a4883166ee7f078cb4
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-08 17:45:32 +0300

    Play only one file at a time

--- Hurmamusic/Hurmamusic/wwwroot/js/site.js
+++ Hurmamusic/Hurmamusic/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
